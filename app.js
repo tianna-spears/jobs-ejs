5dcc330a62208a98b41c7300f19e72ef7e11636d
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-11 11:52:22 +0100

    created first EJS file, views/partial folder with head.ej, header.ejs, footer.ejs, secretWord.ejs, and sessions update

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require("express");
 require("express-async-errors");
 const app = express();
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 
-// secret word handling
-let secretWord = "syzygy";
-
 // CRUD functionality
 app.get("/secretWord", (req, res) => {
-  res.render("secretWord", { secretWord });
-})
-
+  if (!req.session.secretWord) {
+    req.session.secretWord = "syzygy";
+  }
+  res.render("secretWord", { secretWord: req.session.secretWord });
+});
 app.post("/secretWord", (req, res) => {
-  secretWord = req.body.secretWord;
+  req.session.secretWord = req.body.secretWord;
   res.redirect("/secretWord");
 });
 
