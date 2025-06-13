91c94becd7cf9376214bd1748fe0fb891a03d091
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-13 09:37:58 +0100

    added sessionRoutes and sessionController, along with middleware to parseValidationErrs

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -25,22 +24,15 @@ const UserSchema = new mongoose.Schema({
     },
 })
 
-UserSchema.pre('save', async function () {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-})
+// UserSchema.pre('save', async function () {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt)
+// })
 
-UserSchema.methods.createJWT = function () {
-    return jwt.sign(
-        { userId: this._id, name: this.name}, 
-        process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_LIFETIME})
-}
 
-UserSchema.methods.comparePassword = async function (candidatePassword) {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password)
-    return isMatch;
-
-}
+// UserSchema.methods.comparePassword = async function (candidatePassword) {
+//     const isMatch = await bcrypt.compare(candidatePassword, this.password)
+//     return isMatch;
+// }
 
 module.exports = mongoose.model('User', UserSchema) 
