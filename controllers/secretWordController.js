03fd00b56cf0d07e05d76b9091bc06ccba12f384
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-20 09:32:43 +0100

    debugged and fixed secretWordRoute and secretWordController. app is now fully functional. also updated npm packagaes that were deprecated.

--- controllers/secretWordController.js
+++ controllers/secretWordController.js
@@ -1,21 +1,26 @@
-const express = require('express')
+const express = require('express');
+const { create } = require('../models/User');
 const app = express()
 // const { csrf } = require('../middleware/csrf')
 
 const getSecretWord = async (req, res) => {
   if (!req.session.secretWord) {
     req.session.secretWord = "syzygy";
-  // const token = csrf.token(req, res);
-  res.render("secretWord", { secretWord: req.session.secretWord, 
-    // _csrf: token };
-})}}
+  }
+  res.render("secretWord", { 
+    secretWord: req.session.secretWord
+  }) 
+}
 
 const createSecretWord=  (req, res) => {
-  if (req.body.secretWord.toUpperCase()[0] == "P") {
+  const createdWord= req.body.secretWord;
+
+  if (createdWord && createdWord.toUpperCase().startsWith("P")) {
     req.flash("error", "That word won't work!");
-    req.flash("error", "You can't use words that start with p.");
-  } else {
-    req.session.secretWord = req.body.secretWord;
+    req.flash("error", "You can't use a word that starts with p.");
+  } 
+  else {
+    req.session.secretWord = createdWord
     req.flash("info", "The secret word was changed.");
   }
   res.redirect("/secretWord");
